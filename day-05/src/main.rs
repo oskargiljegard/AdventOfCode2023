784818f822b87938963134fd993dcdd0890bc4f4
Oskar <oskar@giljegard.com>
2023-12-08 20:24:10 +0100

    Finished day 5 part 2 with brute force, Computers are FAST

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::Range;
+use std::{ops::Range, cmp::min};
 
 #[derive(Debug)]
 struct MapSection {
@@ -73,68 +73,29 @@ fn seeds_to_seed_ranges(seeds: &Vec<u64>) -> Vec<Range<u64>> {
     ranges
 }
 
-fn apply_map_to_range(map: &Map, range: Range<u64>) -> Vec<Range<u64>> {
-    let relevant_sections = map.sections.iter().filter_map(|section| {
-        if section.source_start < range.end || section.source_start + section.length > range.start {
-            return Some((section.source_start..(section.source_start + section.length),
-                        section.destination_start..(section.destination_start + section.length)));
-        }
-        return None;
-    }).collect::<Vec<(Range<u64>, Range<u64>)>>();
-    let mut clamped_sections = relevant_sections.iter().map(|(source_range, destination_range)| {
-        let start = if source_range.start < range.start {
-            range.start
-        } else {
-            source_range.start
-        };
-        let end = if source_range.end > range.end {
-            range.end
-        } else {
-            source_range.end
-        };
-        let start_delta = (start as i128) - (source_range.start as i128);
-        let end_delta = (end as i128) - (source_range.start as i128);
-        let new_destination_start = (destination_range.start as i128) + start_delta;
-        let new_destination_end = (destination_range.start as i128) + end_delta;
-        (start..end, (new_destination_start as u64)..(new_destination_end as u64))
-    }).collect::<Vec<(Range<u64>, Range<u64>)>>();
-    clamped_sections.sort_by_key(|(source_range, _)| source_range.start);
-    let mut out_ranges: Vec<(Range<u64>, Range<u64>)> = Vec::new();
-    let mut last_end = range.start;
-    for (source_range, destination_range) in clamped_sections {
-        if source_range.start > last_end {
-            out_ranges.push((last_end..source_range.start, last_end..source_range.start));
-        }
-        out_ranges.push((source_range.clone(), destination_range));
-        last_end = source_range.end;
-    }
-    if range.end > last_end {
-        out_ranges.push((last_end..range.end, last_end..range.end));
-    }
-    out_ranges.into_iter().map(|(src, dst)| dst).collect::<Vec<_>>()
-}
-
-fn apply_map_to_ranges(map: &Map, ranges: Vec<Range<u64>>) -> Vec<Range<u64>> {
-    ranges.into_iter().flat_map(|range| apply_map_to_range(map, range)).collect::<Vec<_>>()
-}
-
-fn apply_maps_to_ranges(maps: &Vec<Map>, start_ranges: &Vec<Range<u64>>) -> Vec<Range<u64>> {
-    maps.iter().fold(start_ranges.clone(), |ranges, map| apply_map_to_ranges(map, ranges))
-}
 
 fn part2() {
-    let input = include_str!("./input-mini.txt");
+    let input = include_str!("./input1.txt");
     let parts = input.split("\n\n").collect::<Vec<_>>();
     if parts.len() == 0 {
         panic!("Input is in the wrong format");
     }
     let seeds = parse_seeds(parts[0]);
-    let all_seed_ranges = seeds_to_seed_ranges(&seeds).into_iter().map(|range| vec![range]).collect::<Vec<_>>();
+    let all_seed_ranges = seeds_to_seed_ranges(&seeds);
     let maps = parts[1..].iter().map(|s| parse_map(s)).collect::<Vec<_>>();
-    //dbg!(&all_seed_ranges);
-    //dbg!(apply_map_to_range(&maps[0], 0..100));
-    let final_ranges = apply_maps_to_ranges(&maps, &all_seed_ranges[0]);
-    dbg!(&final_ranges);
+    let mut lowest_output = u64::MAX;
+    let total_num_seeds: u64 = all_seed_ranges.iter().map(|range| &range.end - &range.start).sum();
+    let mut num_seeds_tried = 0;
+    dbg!(total_num_seeds);
+    for range in all_seed_ranges.into_iter() {
+        for seed in range {
+            let output = apply_maps(&maps, seed);
+            lowest_output = min(lowest_output, output);
+            num_seeds_tried += 1;
+        }
+        dbg!(num_seeds_tried);
+    }
+    println!("Part 2: {}", lowest_output);
 }
 
 fn main() {
