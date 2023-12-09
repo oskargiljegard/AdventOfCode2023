d0dee27a67ea45122e0e1792fa202124ca0c7391
Oskar <oskar@giljegard.com>
2023-12-09 22:53:19 +0100

    Finished day 8 part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::empty};
 
 fn parse(input: &str) -> (&str, HashMap<&str, (&str, &str)>) {
     let (instr_str, nodes_str) = input.split_once("\n\n").unwrap();
@@ -11,12 +11,12 @@ fn parse(input: &str) -> (&str, HashMap<&str, (&str, &str)>) {
     return (instr_str, map);
 }
 
-fn find_path_length<F>(instr_str: &str, map: HashMap<&str, (&str, &str)>, start_state: &str, is_end_state: F) -> usize 
+fn find_path_length<F>(instr_str: &str, map: HashMap<&str, (&str, &str)>, start_state: &str, is_end_state: F) -> (usize, String)
                         where F: Fn(&str) -> bool {
     let num_instructions = instr_str.chars().count();
     let mut state = start_state;
     let mut i = 0;
-    while !is_end_state(state) {
+    while !is_end_state(state) || i == 0 {
         let action = instr_str.chars().nth(i % num_instructions).unwrap();
         let next = map.get(state).unwrap();
         state = match action {
@@ -26,24 +26,47 @@ fn find_path_length<F>(instr_str: &str, map: HashMap<&str, (&str, &str)>, start_
         };
         i += 1;
     }
-    return i;
+    return (i, state.to_string());
+}
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    (a * b) / gcd(a, b)
 }
 
 fn part1() {
     let input = include_str!("input1.txt");
     let (instr_str, map) = parse(input);
-    let num_steps = find_path_length(instr_str, map, "AAA", |s| s=="ZZZ");
+    let (num_steps, _) = find_path_length(instr_str, map, "AAA", |s| s=="ZZZ");
     println!("Part 1: {}", num_steps);
 }
 
 fn part2() {
     let input = include_str!("input1.txt");
     let (instr_str, map) = parse(input);
-    let num_steps = find_path_length(instr_str, map, "AAA", |s| s=="ZZZ");
-    println!("Part 1: {}", num_steps);
+    let start_states = map.keys().filter(|k| k.ends_with("A")).collect::<Vec<_>>();
+    let mut all_steps: Vec<i64> = vec![];
+    for start_state in start_states {
+        let (num_steps, final_state) = find_path_length(instr_str, map.clone(), start_state, |s| s.ends_with("Z"));
+        let (period, final_state2) = find_path_length(instr_str, map.clone(), &final_state, |s| s.ends_with("Z"));
+        assert!(final_state == final_state2);
+        println!("Start state: {} Final state: {} Steps: {} Period: {}", start_state, final_state, num_steps, period);
+        all_steps.push(num_steps as i64);
+    }
+    let lcm = all_steps.iter().fold(1, |acc, x| lcm(acc, *x as u64));
+    println!("Part 2: {}", lcm);
 }
 
 
 fn main() {
     part1();
+    part2();
 }
